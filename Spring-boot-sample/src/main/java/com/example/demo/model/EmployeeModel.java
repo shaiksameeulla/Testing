3bf570a4c635a8b0bc4caa84a8e6af114095e57f
samiDeveloper <sami.mca3@gmail.com>
2018-03-17 22:41:12 +0530

    Added EmployeeFeature

---
+++ Spring-boot-sample/src/main/java/com/example/demo/model/EmployeeModel.java
@@ -0,0 +1,27 @@
+package com.example.demo.model;
+
+import java.io.Serializable;
+
+public class EmployeeModel implements Serializable{
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 4235588409979331384L;
+	private String name;
+	private String title;
+	public String getName() {
+		return name;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	
+
+}
